47972b4b8e306e304e6b221f600fadb630dc206c
Shokoufeh Yazdanian <shokufe.yazdanian@gmail.com>
2026-07-20 14:59:52 +0200

    Update validate_orders.py

--- src/validation/validate_orders.py
+++ src/validation/validate_orders.py
@@ -6,12 +6,18 @@ context = gx.get_context()
 def validate_orders():
 
     batch = context.get_validator(
-        datasource_name="postgres_datasource", data_asset_name="olist_orders_dataset"
+        datasource_name="postgres_datasource",
+        data_asset_name="olist_orders_dataset",
     )
 
-    batch.expect_table_row_count_to_be_between(min_value=90000, max_value=110000)
+    batch.expect_table_row_count_to_be_between(
+        min_value=90000,
+        max_value=110000,
+    )
 
-    batch.expect_column_values_to_not_be_null(column="order_id")
+    batch.expect_column_values_to_not_be_null(
+        column="order_id",
+    )
 
     result = batch.validate()
 
